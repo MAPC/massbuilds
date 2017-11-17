99a0eb089680189f86d98d3434a23dccad3dcc0c
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-11-17 16:49:37 -0500

    Add Rails Facility for Ember Simple Auth
    This commit follows the tutorial at https://romulomachado.github.io/2015/09/28/using-ember-simple-auth-with-devise.html to address part of #2 by implementing the rails side authentication for Ember.js.

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:admin, :verified, :researcher, :user]
+  before_save :ensure_authentication_token
+
+  private
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
 end
--- rails/config/application.rb
+++ rails/config/application.rb
@@ -22,5 +22,12 @@ module Massbuilds
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.generators.javscript_engine :js
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
