0e5ed2902a2e9c0501a1b4ba4a28999dae8dd9b9
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-11-29 15:43:11 -0500

    WIP Municipality Search
    Adds a municipality field for developments and then a rake task to populate the municipality field based on Postgres ST_INTERSECTS function.
    
    Also updates SRID of the development POINT to be correct for doing transforms and projections.

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117210007) do
+ActiveRecord::Schema.define(version: 20171129181217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,13 +66,14 @@ ActiveRecord::Schema.define(version: 20171117210007) do
     t.decimal "longitude"
     t.string "parcel_id"
     t.boolean "mixed_use"
-    t.geometry "point", limit: {:srid=>0, :type=>"st_point"}
+    t.geometry "point", limit: {:srid=>4326, :type=>"st_point"}
     t.string "programs"
     t.boolean "forty_b"
     t.boolean "residential"
     t.boolean "commercial"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "municipality"
   end
 
   create_table "ma_municipalities", primary_key: "gid", id: :serial, force: :cascade do |t|
@@ -80,6 +81,7 @@ ActiveRecord::Schema.define(version: 20171117210007) do
     t.string "municipal", limit: 35
     t.geometry "geom", limit: {:srid=>26986, :type=>"multi_polygon"}
     t.index ["geom"], name: "ma_municipalities_geom_idx", using: :gist
+    t.index ["geom"], name: "ma_municipalities_geom_idx1", using: :gist
   end
 
   create_table "users", force: :cascade do |t|
