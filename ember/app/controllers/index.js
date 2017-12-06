dfd4d22705e667455c050ecb7bc0b2b626704b33
Eric Youngberg <eyoungberg@mapc.org>
2017-12-06 15:25:59 -0500

    Increase the color experience

--- ember/app/controllers/index.js
+++ ember/app/controllers/index.js
@@ -1,13 +1,15 @@
 import Controller from '@ember/controller';
 import { service } from 'ember-decorators/service';
 
+
 export default class extends Controller {
 
   @service map
 
+
   constructor() {
     super();
 
-    this.searchPlaceholder = 'Search by Town/City, Developer, Address...'
+    this.searchPlaceholder = 'Search by Town/City, Developer, Address...';
   }
 }
