b3c61dd87ef5d1f9c71ddb4365ddef75ecc1e94f
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-12-26 17:14:11 -0500

    Implement Edit Model Validations
    * Implement validations for the edit model using JSON schema.
    
    * Fix bug in the Development validation model that caused groundbroken? to always resolve to true

--- rails/spec/factories/edits.rb
+++ rails/spec/factories/edits.rb
@@ -2,9 +2,24 @@ FactoryBot.define do
   factory :edit do
     user
     proposed_changes { { name: 'Test Development',
-                         tagline: "new tagline",
-                         state: "CT",
-                         hotelrms: 2
+                         tagline: 'new tagline',
+                         hotelrms: 2,
+                         year_compl: 1987,
+                         yrcomp_est: "1987",
+                         status: 'projected',
+                         address: '23 Banks St',
+                         zip_code: '02138',
+                         hu: 1,
+                         commsf: 25,
+                         desc: 'A sample edited development',
+                         rdv: true,
+                         asofright: false,
+                         clusteros: false,
+                         phased: false,
+                         stalled: false,
+                         mixed_use: true,
+                         headqtrs: false,
+                         ovr55: false
                      } }
   end
 end
