2c9bebc2f8800b81567243d1947f2ea116c2e281
Eric Youngberg <eyoungberg@mapc.org>
2017-12-27 10:56:35 -0500

    Flesh out user account subpanel

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
+
 export default Route.extend(ApplicationRouteMixin, {
 
   currentUser: Ember.inject.service(),
@@ -19,9 +20,8 @@ export default Route.extend(ApplicationRouteMixin, {
 
 
   _loadCurrentUser() {
-    return this.get('currentUser').load().catch((e) => {
-      console.log(e);
-      //this.get('session').invalidate()
+    return this.get('currentUser').load().catch(() => {
+      this.get('session').invalidate();
     });
   }
 
