07fb6b7ff86c6ec6e956d9f3f4f416dacb8563af
Eric Youngberg <eyoungberg@mapc.org>
2017-12-27 13:03:51 -0500

    Nest developments functionality under map route

--- ember/app/router.js
+++ ember/app/router.js
@@ -7,7 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('developments');
+  this.route('map', function() {
+    this.route('developments');
+  });
 });
 
 export default Router;
---
+++ ember/app/routes/map.js
@@ -0,0 +1,70 @@
+import Route from '@ember/routing/route';
+import { action } from 'ember-decorators/object';
+import { service } from 'ember-decorators/service';
+import { filters, fromQueryParams } from 'massbuilds/utils/filters';
+
+
+export default class extends Route {
+
+  @service map
+
+
+  model(params) {
+    const query = { trunc: true };
+
+    if (params) {
+      query['filter'] = fromQueryParams(this.getFilterParams(params));
+    }
+
+    this.get('map').filterByQuery(query);
+  }
+
+
+  /**
+   * We need to allow arrays in the query parameters
+   * for the discrete filters.
+   */
+  serializeQueryParam(value, urlKey, defaultValueType) {
+    if (defaultValueType === 'array') {
+      return value;
+    }
+    
+    return '' + value; 
+  }
+
+  deserializeQueryParam(value, urlKey, defaultValueType) {
+    if (defaultValueType === 'array' && typeof value === 'string') {
+      return value.split(',');
+    }
+
+    return value; 
+  }
+
+
+  @action
+  refreshModel() {
+    this.refresh();
+  }
+
+
+  getFilterParams(params) {
+    const setParams = {};
+
+    Object.keys(params).forEach(key => {
+      if (key in filters) {
+       const value = params[key];
+
+        if (
+          value 
+          && (!(value instanceof Array) || value.length > 0)) 
+        {
+          setParams[key] = value;
+        }
+      }
+    });
+
+    return setParams;
+  }
+
+
+}
