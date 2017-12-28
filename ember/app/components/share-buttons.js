5e04602a7df369ae594ec332af7578c41614706b
Eric Youngberg <eyoungberg@mapc.org>
2017-12-28 14:47:04 -0500

    Implement link sharing button

--- ember/app/components/share-buttons.js
+++ ember/app/components/share-buttons.js
@@ -9,11 +9,79 @@ export default class extends Component {
 
     this.classNames = ['component', 'share-buttons'];
     this.isOpen = false;
+
+    this.successLinkTimer = null;
+    this.linkMessage = null;
   }
+
 
   @action
   toggleOpen() {
+    const timer = this.get('successLinkTimer');
+
     this.toggleProperty('isOpen');
+    this.set('linkMessage', null);
+
+    if (timer) {
+      clearTimeout(timer);
+    }
   }
 
+
+  @action
+  toLink() {
+    const copyArea = document.createElement('textarea');
+    copyArea.class = 'copy-area';
+    copyArea.innerHTML = window.location.href
+                                        .split('&municipality[]=').join(',')
+                                        .split('&developerName[]=').join(',')
+                                        .split('[]=').join('=')
+                                        .split('%3B').join(';');
+
+    document.querySelector('body').appendChild(copyArea);
+    copyArea.select();
+
+    try {
+      const copied = document.execCommand('copy');
+
+      if (copied) {
+        this.successLinkShare();
+      }
+    }
+    catch(e) {
+      console.log(e);
+    }
+
+    copyArea.innerHTML = '';
+  }
+
+
+  @action
+  toShapefile() {
+  
+  }
+
+  @action
+  toGeoJSON() {
+  
+  }
+
+  @action
+  toCSV() {
+  
+  }
+
+
+  successLinkShare() {
+    const timer = this.get('successLinkTimer');
+    this.set('linkMessage', 'Copied Link');
+    
+    if (timer)  {
+      clearTimeout(timer);
+    }
+
+    this.set('successLinkTimer', setTimeout(() => {
+      this.set('linkMessage', null);
+    }, 3000));
+  }
 }
