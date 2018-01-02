6511685ea8fc0f05665e61950c534595207731c9
Eric Youngberg <eyoungberg@mapc.org>
2018-01-02 08:41:27 -0500

    Generate attributes list for developments in controller

--- ember/app/controllers/map/developments/development.js
+++ ember/app/controllers/map/developments/development.js
@@ -7,8 +7,39 @@ export default class extends Controller {
 
   @service map
 
-  constructor() {
-    super();
+
+  @computed('model')
+  get keyAttributes() {
+    return this.attributesFor({
+      rdv: 'Redevelopment',
+      phased: 'Phased',
+      stalled: 'Stalled',
+      asofright: 'As of Right',
+      mixedUse: 'Mixed Use',
+    });
   }
+
+
+  @computed('model')
+  get residentialAttributes() {
+    return this.attributesFor({
+      ovr55: 'Age Restricted' ,
+      private: 'Private',
+    });
+  }
+
+
+  attributesFor(attributeDict) {
+    const attributes = [];
+    const model = this.get('model');
+
+    Object.keys(attributeDict).forEach(attribute => {
+      if (model.get(attribute)) {
+        attributes.push(attributeDict[attribute]);
+      }
+    });
+
+    return attributes.join(', ');
+ }
 
 }
