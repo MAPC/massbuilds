17c7a00d772a96d75c57a45dd268027234905403
Eric Youngberg <eyoungberg@mapc.org>
2018-03-08 15:45:37 -0500

    Fix bug where user fullNames aren't loading into development panel

--- rails/app/serializers/development_serializer.rb
+++ rails/app/serializers/development_serializer.rb
@@ -1,15 +1,17 @@
 class DevelopmentSerializer < ActiveModel::Serializer
-[:id, :name, :status, :address, :nhood, :municipal, :devlper, :latitude, :longitude].each { |attr| attribute attr }
-[:user_id, :rdv, :asofright, :ovr55, :clusteros, :phased, :stalled,
-  :descr, :prj_url, :state, :zip_code, :height,
-  :stories, :year_compl, :prjarea, :singfamhu, :smmultifam, :lgmultifam,
-  :hu, :gqpop, :rptdemp, :estemp, :commsf, :hotelrms, :onsitepark,
-  :total_cost, :ret_sqft, :rpa_name, :county, :n_transit,
-  :ofcmd_sqft, :indmf_sqft, :whs_sqft, :rnd_sqft, :ei_sqft, :other_sqft,
-  :hotel_sqft, :other_rate, :affordable, :parcel_id, :mixed_use, :point,
-  :programs, :forty_b, :residential, :commercial, :yrcomp_est, :units_1bd,
-  :units_2bd, :units_3bd, :affrd_unit, :aff_u30, :aff_30_50, :aff_50_80,
-  :aff_80p, :headqtrs, :park_type, :publicsqft, :unknownhu, :unk_sqft, :aff_unknown, :updated_at].each { |attr| attribute attr, unless: :truncated? }
+  belongs_to :user
+
+  [:id, :name, :status, :address, :nhood, :municipal, :devlper, :latitude, :longitude].each { |attr| attribute attr }
+  [:user_id, :rdv, :asofright, :ovr55, :clusteros, :phased, :stalled,
+    :descr, :prj_url, :state, :zip_code, :height,
+    :stories, :year_compl, :prjarea, :singfamhu, :smmultifam, :lgmultifam,
+    :hu, :gqpop, :rptdemp, :estemp, :commsf, :hotelrms, :onsitepark,
+    :total_cost, :ret_sqft, :rpa_name, :county, :n_transit,
+    :ofcmd_sqft, :indmf_sqft, :whs_sqft, :rnd_sqft, :ei_sqft, :other_sqft,
+    :hotel_sqft, :other_rate, :affordable, :parcel_id, :mixed_use, :point,
+    :programs, :forty_b, :residential, :commercial, :yrcomp_est, :units_1bd,
+    :units_2bd, :units_3bd, :affrd_unit, :aff_u30, :aff_30_50, :aff_50_80,
+    :aff_80p, :headqtrs, :park_type, :publicsqft, :unknownhu, :unk_sqft, :aff_unknown, :updated_at].each { |attr| attribute attr, unless: :truncated? }
 
   def truncated?
     scope == 'trunc'
