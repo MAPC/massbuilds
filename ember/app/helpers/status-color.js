6a750767447087df7da2b4922088b868d1d3c4bd
Eric Youngberg <eyoungberg@mapc.org>
2018-04-23 11:24:33 -0400

    Escapea styles

--- ember/app/helpers/status-color.js
+++ ember/app/helpers/status-color.js
@@ -1,9 +1,10 @@
+import Ember from 'ember';
 import { helper } from '@ember/component/helper';
 import statusColors from 'massbuilds/utils/status-colors';
 
 
 export function statusColor(params) {
-  return statusColors[params[0]] || 'grey';
+  return new Ember.String.htmlSafe(statusColors[params[0]] || 'grey');
 }
 
 export default helper(statusColor);
