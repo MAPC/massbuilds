cd4c31fe86143f9d92847749811e99952da2560a
Eric Youngberg <eyoungberg@mapc.org>
2018-04-26 15:42:22 -0400

    Wrap IF all of the things

--- rails/db/migrate/20180208153707_change_affordable_units_unknown_type_column_name.rb
+++ rails/db/migrate/20180208153707_change_affordable_units_unknown_type_column_name.rb
@@ -1,5 +1,5 @@
 class ChangeAffordableUnitsUnknownTypeColumnName < ActiveRecord::Migration[5.1]
   def change
-    rename_column :developments, :affUnknown, :aff_unknown
+    rename_column :developments, :affUnknown, :aff_unknown if ActiveRecord::Base.connection.column_exists? :developments, :affUnknown
   end
 end
