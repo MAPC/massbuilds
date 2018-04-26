aac479f459a04de4a54e264282daf39c2e5b8449
Eric Youngberg <eyoungberg@mapc.org>
2018-04-26 13:58:31 -0400

    Change mailgun gem to official mailgun-ruby gem

--- rails/config/environments/staging.rb
+++ rails/config/environments/staging.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    'api_key' => Rails.application.secrets.mailgun_api_key,
-    'domain' => 'mailgun2.mapc.org'
+    api_key: Rails.application.secrets.mailgun_api_key,
+    domain: 'mailgun2.mapc.org'
   }
 
   # Use a different logger for distributed setups.
