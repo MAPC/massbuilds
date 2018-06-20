722f8ab3f660d45f36cd4832fe74327cd5247757
Ian Hill <ihill@mapc.org>
2018-06-20 09:10:21 -0400

    Fix development relationships in user serialization

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
 
   # GET /users
   def index
