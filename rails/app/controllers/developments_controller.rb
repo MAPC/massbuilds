5daaef0e5e3d72be67ec57e94e5efbce9219b1b5
Ian Hill <ihill@mapc.org>
2018-06-20 15:06:25 -0400

    New development creation experience and updated geocoding

--- rails/app/controllers/developments_controller.rb
+++ rails/app/controllers/developments_controller.rb
@@ -55,7 +55,10 @@ class DevelopmentsController < ApplicationController
         format.jsonapi { render json: FullDevelopmentSerializer.new(@development).serialized_json }
       end
     else
-      render json: FullDevelopmentSerializer.new(@development.errors.full_messages).serialized_json, status: :bad_request
+      puts @development.errors.full_messages
+      respond_to do |format|
+        format.jsonapi { head :bad_request }
+      end
     end
   end
 
