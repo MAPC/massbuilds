47008c4eebbe63243bf3ca8a5a8ead8a1f285d11
Ian Hill <ihill@mapc.org>
2018-06-26 13:17:42 -0400

    Prevent zooming when a user tries to create a new development from the search bar

--- ember/app/components/mapbox-map.js
+++ ember/app/components/mapbox-map.js
@@ -274,11 +274,13 @@ export default class extends Component {
     const data = mapService.get('filteredData').length
         ? mapService.get('filteredData')
         : mapService.get('stored');
-    const fitBounds = data.reduce(
-    (bounds, datum) => bounds.extend([datum.get('longitude'), datum.get('latitude')]),
-      new mapboxgl.LngLatBounds()
-    );
-    this.mapboxglMap.fitBounds(fitBounds, { padding: 40 });
+    if (data.toArray().length > 0) {
+      const fitBounds = data.reduce(
+      (bounds, datum) => bounds.extend([datum.get('longitude'), datum.get('latitude')]),
+        new mapboxgl.LngLatBounds()
+      );
+      this.mapboxglMap.fitBounds(fitBounds, { padding: 40 });
+    }
   }
 
   generatePaintProperties(selected, highContrast, isMuted) {
