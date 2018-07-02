6849fb544c8bb2ce23f3f30fb96d4b1c4498e4c5
Ian Hill <ihill@mapc.org>
2018-07-02 10:30:51 -0400

    Sort glossary

--- ember/app/controllers/glossary.js
+++ ember/app/controllers/glossary.js
@@ -4,6 +4,8 @@ import content from 'massbuilds/content';
 export default class extends Controller {
   constructor() {
     super();
-    this.glossaryTerms = Object.values(content.GLOSSARY);
+    this.glossaryTerms = Object.values(content.GLOSSARY)
+        .sort((term1, term2) =>
+            term1.label.toLowerCase().localeCompare(term2.label.toLowerCase()));
   }
 }
