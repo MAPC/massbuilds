a98b148f8e36b0e5805a18061f07bde15f90315f
Ian Hill <ihill@mapc.org>
2018-07-10 14:00:04 -0400

    Compact meta data and hide export buttons

--- ember/app/components/share-buttons.js
+++ ember/app/components/share-buttons.js
@@ -11,7 +11,7 @@ export default class extends Component {
     super();
 
     this.classNames = ['component', 'share-buttons'];
-
+    this.classNameBindings = ['hidden'];
     this.linkTimer = null;
     this.linkMessage = null;
 
