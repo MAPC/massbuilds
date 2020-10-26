40a9549f5fc8261304e825666fe6d01a14380298
Matthew Zagaja <mzagaja@mapc.org>
2020-10-26 16:10:29 -0400

    Upgrade to Ember 3.0.0
    We ran `ember-cli-update —to 3.0.0` and npm update in order to bring us into the world of Ember 3!

--- ember/.eslintrc.js
+++ ember/.eslintrc.js
@@ -13,5 +13,22 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  rules: {},
+  overrides: [
+    {
+      files: [
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'lib/*/index.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015,
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+    },
+  ],
 };
