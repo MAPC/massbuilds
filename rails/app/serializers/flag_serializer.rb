3235773c534e00b0ae4533b16c7713d16d22d51e
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-11-13 10:38:33 -0500

    Add updated_at to serializer to fix 500 response in patch request
    We had a route to patch a flag, but Rails was not able to see the updated_at attribute, causing a 500 error. Adding updated_at to the serializer makes this attribute accessible, meaning that you can now unflag a development/set is_resolved to True from the UI
    
    Co-authored-by: Matt Zagaja <mzagaja@mapc.org>

--- rails/app/serializers/flag_serializer.rb
+++ rails/app/serializers/flag_serializer.rb
@@ -8,4 +8,8 @@ class FlagSerializer < ActiveModel::Serializer
   attribute :created_at do
     object.created_at.iso8601
   end
+
+  attribute :updated_at do
+    object.updated_at.iso8601
+  end
 end
