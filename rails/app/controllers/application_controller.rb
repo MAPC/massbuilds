24db80d4fa550fe5166cbaa2be4cd67adf0d7c1b
Matthew Zagaja <mzagaja@mapc.org>
2020-11-17 11:18:45 -0500

    Resolve #245 User Selected Password Reset
    * Remove verify_authorized because the devise controller does not check for authorization and it was throwing an error and we do not wish to spend our lives overriding devise with pundit methods.
    
    * Update password_resets_controller.rb to use the built-in Devise method for user password resets so the user can pick their own password. Unlike the current implementation it will bring them to a page to pick their password of choice. They will get an email with a URL that is unique to them so they can do this.
    
    Co-Authored By: Annabelle Taylor <ataylor@mapc.org>

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user_from_token!
   before_action :authenticate_user!
-  after_action :verify_authorized
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
