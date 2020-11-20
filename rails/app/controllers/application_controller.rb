4bd5f6a756b60db91406412ecddccf3971b795d8
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-11-20 13:40:20 -0500

    Remove protect_from_forgey with :null_session
    This was throwing an error wherein the current_user was not maintained across an action (such as unflagging a development)
    
    Co-Authored by: Matt Zagaja <mzagaja@mapc.org>

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user_from_token!
   before_action :authenticate_user!
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
     authenticate_with_http_token do |token, options|
       user_email = options[:email].presence
       user = user_email && User.find_by_email(user_email)
-
       if user && Devise.secure_compare(user.authentication_token, token)
         sign_in user, store: false
       end
